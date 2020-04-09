84c6a851e389bf7d205a4b0babbe11325132a0f1
santiago <santiago.orozco@sundevs.com>
2020-04-09 17:43:41 -0500

    hotfix01

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log ("¡El desarrollo basado en troncales es increíble!");
 console.log ("La funcionalidad 3");
-console.log ("Permite una iteración rápida!"); // # nueva línea agregada 
+console.log ("Permite una iteración sea mas rápida!"); // # nueva línea agregada 
 
